964df65967942208827ba710469babd1ebde1188
tonnahe171051 <tonnahe171051@gmail.com>
2024-11-11 00:42:37 +0700

    feat: apply dependence injection for repository

--- AssetManagement/App.xaml.cs
+++ AssetManagement/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Configuration;
+﻿using AssetManagement.DataAccess;
+using AssetManagement.Repository;
+using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -9,6 +12,33 @@ namespace AssetManagement
     /// </summary>
     public partial class App : Application
     {
+        private ServiceProvider serviceProvider;
+
+        public App()
+        {
+            ServiceCollection services = new ServiceCollection();
+            ConfigureServices(services);
+            serviceProvider = services.BuildServiceProvider();
+        }
+
+        private void ConfigureServices(ServiceCollection services)
+        {
+            services.AddDbContext<AssetUnityManagementContext>();
+            services.AddScoped<AssetService>();
+            services.AddScoped<TagService>();
+            services.AddScoped<FileService>();
+            services.AddScoped<ITagRepository, TagRepository>();
+            services.AddScoped<IAssetRepository, AssetRepository>();
+            services.AddScoped<IFileRepository, FileRepository>();
+            services.AddTransient<MainWindow>();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            var mainWindow = serviceProvider.GetService<MainWindow>();
+            mainWindow.Show();
+        }
     }
 
 }
